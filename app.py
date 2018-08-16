25386d079fc93a9aea05227f056b572b6410f233
F74046080 <for1426@gmail.com>
2018-08-16 15:56:54 +0800

    Add the html(render)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -11,23 +11,21 @@ app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 # models
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=True)
     cost = db.Column(db.Integer, nullable=True)
 
-
 # views
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 
 @app.route("/name")
 def name():
-    return "Hello Frank"
+    return "Hello Apple"
 
 @app.route("/record", methods=['POST'])
 def add_record():
